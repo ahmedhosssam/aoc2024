2abd2e2fc3cee583598c75c64b343caa1b044268
Ahmed Hossam <hoss4614@gmail.com>
2024-12-01 21:03:59 +0200

    day1

--- day01b.py
+++ day01b.py
@@ -1,20 +1,13 @@
 n = int(input())
-
 a = []
 mp = {}
 
 for _ in range(n):
     x, y = map(int, input().split())
     a.append(x)
-    if y in mp:
-        mp[y]+=1
-    else:
-        mp[y]=1
+    if y not in mp: mp[y]=0 
+    mp[y]+=1
 
-res = 0
-
-for x in a:
-    if x in mp:
-        res += x*mp[x]
+res = sum(x*mp[x] if x in mp else 0 for x in a)
 
 print(res)
